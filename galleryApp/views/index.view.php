80dd2a88559d73663e49b9231f2c2a285bf1f2d2
ArruCG <canchalangel@gmail.com>
2021-02-23 00:03:25 +0100

    commit frames Gmaps y SQL actualizado

--- galleryApp/views/index.view.php
+++ galleryApp/views/index.view.php
@@ -21,6 +21,7 @@
                     
                 </form>
             </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3101.236175363125!2d-3.9309490843639066!3d38.98710587955697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6bc36a56ec2803%3A0x124578e9147bc240!2sCalle%20de%20Toledo%2C%2011%2C%2013001%20Ciudad%20Real%2C%20Cdad.%20Real!5e0!3m2!1ses!2ses!4v1614032855409!5m2!1ses!2ses" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     </header>
 </body>
